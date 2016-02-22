49e0e54775e144252d11542fa48e8db2019c5c8a
Greg V <greg@unrelenting.technology>
2016-02-22 21:26:57 +0300

    add periodic measurement widget

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,9 +1,17 @@
 extern crate unixbar;
+extern crate systemstat;
 use unixbar::*;
+use systemstat::{System, Platform};
 
 fn main() {
     UnixBar::new(LemonbarFormatter::new())
-        .add(Text::new(Format::FgColor("#11bb55".to_owned(), Box::new(Format::Str("HI".to_owned())))))
-        .add(Wrap::new(|f| Format::FgColor("#bb1155".to_owned(), Box::new(f)), DateTime::new("%Y-%m-%d %H:%M:%S %z")))
+        .add(Text::new(Format::FgColor("#11bb55".to_owned(), Box::new(Format::Str("HI ".to_owned())))))
+        .add(Wrap::new(|f| Format::FgColor("#bb1155".to_owned(), Box::new(f)),
+                       DateTime::new("%Y-%m-%d %H:%M:%S %z")))
+        .add(Periodic::new(Duration::from_secs(2), || {
+            let mem = System::new().memory().unwrap();
+            Format::Str(format!(" {} free, {} active",
+                                mem.free.to_string(true), mem.active.to_string(true)))
+        }))
         .run();
 }
---
+++ src/widget/periodic.rs
@@ -0,0 +1,45 @@
+use std::sync::mpsc::Sender;
+pub use std::time::Duration;
+use std::thread;
+use std::sync::{Arc, RwLock};
+use super::base::Widget;
+use format::data::Format;
+
+pub struct Periodic<F: Fn() -> Format> {
+    interval: Duration,
+    updater: Arc<Box<F>>,
+    last_value: Arc<RwLock<Format>>,
+}
+
+impl<F> Widget for Periodic<F> where F: Fn() -> Format + Sync + Send + 'static {
+    fn current_value(&self) -> Format {
+        (*self.last_value).read().unwrap().clone()
+    }
+
+    fn spawn_notifier(&mut self, tx: Sender<()>) {
+        let interval = self.interval;
+        let updater = self.updater.clone();
+        let last_value = self.last_value.clone();
+        thread::spawn(move || {
+            loop {
+                thread::sleep(interval);
+                {
+                    let mut writer = last_value.write().unwrap();
+                    *writer = (*updater)();
+                }
+                let _ = tx.send(());
+            }
+        });
+    }
+}
+
+impl<F> Periodic<F> where F: Fn() -> Format {
+    pub fn new(interval: Duration, updater: F) -> Box<Periodic<F>> {
+        let v = updater();
+        Box::new(Periodic {
+            interval: interval,
+            updater: Arc::new(Box::new(updater)),
+            last_value: Arc::new(RwLock::new(v))
+        })
+    }
+}
